5f6c2cb4c551e0202d5a368c541753a213cdd470
NataliaColome <nmcolome@gmail.com>
2018-07-16 12:18:57 -0600

    Add test and logic to print pp for >1 monkeys

--- lib/little_monkeys.rb
+++ lib/little_monkeys.rb
@@ -1,5 +1,33 @@
+require '../lib/numbers.rb'
+
 class LittleMonkeys
-  def run
-    "Three little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\""
+  def run(number)
+    names = NumbersToWords.new
+    song = (2..number).to_a.reverse.map do |num|
+      "#{names.run(num).capitalize} little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\""
+    end
+    song.join("\n\n")
   end
 end
+
+=begin
+Three little monkeys jumping on the bed,
+One fell off and bumped his head,
+Mama called the doctor and the doctor said,
+"No more monkeys jumping on the bed!"
+
+Two little monkeys jumping on the bed,
+One fell off and bumped his head,
+Mama called the doctor and the doctor said,
+"No more monkeys jumping on the bed!"
+
+One little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said,
+"Get those monkeys right to bed!"
+
+One little monkey jumping on the bed,
+He fell off and bumped his head,
+Mama called the doctor and the doctor said,
+"Get those monkeys right to bed!"
+=end
--- test/little_monkeys_test.rb
+++ test/little_monkeys_test.rb
@@ -2,18 +2,20 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require '../lib/little_monkeys.rb'
+# require '../lib/numbers.rb'
 
 class LittleMonkeysTest < Minitest::Test
-  def test_it_exists
+  def skip test_it_exists
     little_monkeys = LittleMonkeys.new
 
     assert_instance_of LittleMonkeys, little_monkeys
   end
 
-  def test_prints_for_3_monkeys
+  def test_prints_2_paragraphs
     little_monkeys = LittleMonkeys.new
-    paragraph = "Three little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\""
 
-    assert_equal paragraph, little_monkeys.run
+    paragraphs = "Three little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\"\n\nTwo little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\""
+
+    assert_equal paragraphs, little_monkeys.run(3)
   end
 end

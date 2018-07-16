6d306fa24614a80c6491da3e7ca78e48952c4a13
NataliaColome <nmcolome@gmail.com>
2018-07-16 16:45:29 -0600

    Refactor and add test for additional numbers

--- test/little_monkeys_test.rb
+++ test/little_monkeys_test.rb
@@ -5,25 +5,25 @@ require '../lib/little_monkeys.rb'
 # require '../lib/numbers.rb'
 
 class LittleMonkeysTest < Minitest::Test
-  def skip test_it_exists
-    little_monkeys = LittleMonkeys.new
-
-    assert_instance_of LittleMonkeys, little_monkeys
+  def setup
+    @little_monkeys = LittleMonkeys.new
   end
 
   def skip test_prints_2_paragraphs
-    little_monkeys = LittleMonkeys.new
-
     paragraphs = "Three little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\"\n\nTwo little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\""
 
-    assert_equal paragraphs, little_monkeys.run(3)
+    assert_equal paragraphs, @little_monkeys.run(3)
   end
 
   def test_prints_whole_song
-    little_monkeys = LittleMonkeys.new
-
     song = "Three little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\"\n\nTwo little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\"\n\nOne little monkey jumping on the bed,\nHe fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"Get those monkeys right to bed!\"\n\nOne little monkey jumping on the bed,\nHe fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"Get those monkeys right to bed!\""
 
-    assert_equal song, little_monkeys.run(3)
+    assert_equal song, @little_monkeys.run(3)
+  end
+
+  def test_prints_for_7_monkeys
+    song = "Seven little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\"\n\nSix little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\"\n\nFive little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\"\n\nFour little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\"\n\nThree little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\"\n\nTwo little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\"\n\nOne little monkey jumping on the bed,\nHe fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"Get those monkeys right to bed!\"\n\nOne little monkey jumping on the bed,\nHe fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"Get those monkeys right to bed!\""
+
+    assert_equal song, @little_monkeys.run(7)
   end
 end

9afacdbc589c5a898463f6756a6b3cc749551a84
NataliaColome <nmcolome@gmail.com>
2018-07-16 16:39:43 -0600

    Create Text class and refactor

--- lib/little_monkeys.rb
+++ lib/little_monkeys.rb
@@ -1,36 +1,30 @@
 require '../lib/numbers.rb'
+require '../lib/text.rb'
 
 class LittleMonkeys
+
   def run(number)
-    names = NumbersToWords.new
-    song = (2..number).to_a.reverse.map do |num|
-      "#{names.run(num).capitalize} little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\""
-    end
+    song = monkeys_paragraphs(number)
+
     2.times do
-      song << "One little monkey jumping on the bed,\nHe fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"Get those monkeys right to bed!\""
+      song << text.final_paragraphs
     end
+
     song.join("\n\n")
   end
+
+  def monkeys_paragraphs(number)
+    (2..number).to_a.reverse.map do |num|
+      names.run(num).capitalize + text.no_more_jumping
+    end
+  end
+
+  def names
+    NumbersToWords.new
+  end
+
+  def text
+    Text.new
+  end
+
 end
-
-=begin
-Three little monkeys jumping on the bed,
-One fell off and bumped his head,
-Mama called the doctor and the doctor said,
-"No more monkeys jumping on the bed!"
-
-Two little monkeys jumping on the bed,
-One fell off and bumped his head,
-Mama called the doctor and the doctor said,
-"No more monkeys jumping on the bed!"
-
-One little monkey jumping on the bed,
-He fell off and bumped his head,
-Mama called the doctor and the doctor said,
-"Get those monkeys right to bed!"
-
-One little monkey jumping on the bed,
-He fell off and bumped his head,
-Mama called the doctor and the doctor said,
-"Get those monkeys right to bed!"
-=end
---
+++ lib/text.rb
@@ -0,0 +1,9 @@
+class Text
+  def no_more_jumping
+    " little monkeys jumping on the bed,\nOne fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"No more monkeys jumping on the bed!\""
+  end
+
+  def final_paragraphs
+    "One little monkey jumping on the bed,\nHe fell off and bumped his head,\nMama called the doctor and the doctor said,\n\"Get those monkeys right to bed!\""
+  end
+end
